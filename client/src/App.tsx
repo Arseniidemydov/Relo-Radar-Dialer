764195fa8913ad5815378d675d26a25824ada51d
Arseniidemydov <124127490+Arseniidemydov@users.noreply.github.com>
2025-12-10 13:58:44 +0100

    style: move dialer to top

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Dialer } from './components/Dialer'
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#020B2D] text-white font-sans flex flex-col items-center justify-center p-4 selection:bg-blue-500/30">
+    <div className="min-h-screen bg-[#020B2D] text-white font-sans flex flex-col items-center justify-start pt-5 p-4 selection:bg-blue-500/30">
       <div className="w-full max-w-lg">
 
         <main>
